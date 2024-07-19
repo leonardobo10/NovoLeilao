f55b84097f753d66a4d88080df7c5a69bf899195
Leonardo Bernardino <leonardobernardinodeandrade@gmail.com>
2024-07-19 10:53:21 -0300

    Funcionalidade Salvar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,9 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.List;
+
 
 
 public class ProdutosDAO {
@@ -23,9 +26,24 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conectaDAO conecta = new conectaDAO();
+        conn = conecta.connectDB();
+        PreparedStatement ps = null;
+        try{
+            ps=conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+            ps.setString(1, produto.getNome());
+            ps.setInt(2, produto.getValor());
+            ps.setString(3, produto.getStatus());
+            ps.executeUpdate();
+            conn.close();
+            ps.close();
+            
+        }
+        catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null,"");
+        }
         
-        
-        //conn = new conectaDAO().connectDB();
+     
         
         
     }
